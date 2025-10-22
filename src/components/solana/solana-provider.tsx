a79a7e3a32ed6d29eb2436607ca934f6fb18c614
Chris Cherniakov <taequn@gmail.com>
2025-10-21 20:46:29 -0700

    feat(idl): copy ts types from solana program folder to frontend folder
    temp solution, must unify everything within monorepo to simplify such things

--- src/components/solana/solana-provider.tsx
+++ src/components/solana/solana-provider.tsx
@@ -21,10 +21,7 @@ interface SolanaProviderProps {
 }
 
 export const SolanaProvider: FC<SolanaProviderProps> = ({ children }) => {
-  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'
   const network = WalletAdapterNetwork.Devnet;
-
-  // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   const wallets = useMemo(
